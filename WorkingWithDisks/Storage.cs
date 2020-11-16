8e015b5a9366da0668bef81715d59f58f69ddc88
Nikita <alarmistgg@gmail.com>
2020-11-16 11:28:45 +0300

    Правки базового класса v2

--- WorkingWithDisks/Storage.cs
+++ WorkingWithDisks/Storage.cs
@@ -9,13 +9,20 @@ namespace WorkingWithDisks
         public string MediaName { get; set; }
         public string Model { get; set; }
 
+        protected Storage(string mediaName, string model)
+        {
+            MediaName = mediaName;
+            Model = model;
+        }
+
+
         //получение объема памяти
-        public abstract void GettingMemorySize();
+        public abstract decimal GettingMemorySize();
         //копирование данных (файлов/папок) на устройство
-        public abstract void CopyDataToDevice();
+        public abstract void CopyDataToDevice(decimal bitfile);
         //получение информации о свободном объеме памяти на устройстве
-        public abstract void SpareMemoryOnTheDevice();
+        public abstract decimal SpareMemoryOnTheDevice();
         //получение общей/полной информации об устройстве
-        public abstract void GetInfoDevice();
+        public abstract string GetInfoDevice();
     }
 }
